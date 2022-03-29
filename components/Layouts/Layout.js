f3ccadf600a62d2fb10ddc8fd4743b2844d800fd
warren yun <wyun2006@gmail.com>
2022-03-29 19:52:28 -0400

    landing page neat

--- components/Layouts/Layout.js
+++ components/Layouts/Layout.js
@@ -1,6 +1,6 @@
 export default function Layout({ children }) {
   return (
-    <div className="bg-black ">
+    <div className="px-12 bg-black">
       <div className="flex items-center justify-center h-screen text-white">
         {children}
       </div>
--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,26 @@
-import Head from 'next/head'
-import Image from 'next/image'
+import Head from "next/head";
+import Image from "next/image";
 
-import Layout from '../components/Layouts/Layout';
+import Layout from "../components/Layouts/Layout";
 
 export default function Home() {
   return (
-    <div>
-      <div className="text-9xl">
-        <span className='text-pink-600 mr-9'>warren</span>
-        <span className='text-cyan-400'>yun</span>
+    <div className="flex flex-row items-center justify-center text-left">
+      <div className="mr-12">
+        <Image src="/warren.png" alt="warren" width={275} height={275} className='rounded-3xl'/>
       </div>
-      
-      <h1 className='text-3xl thin'>beep</h1>
+      <div className="flex flex-col items-start justify-center w-1/3 text-left">
+        <div className="text-8xl">
+          <span className="mr-5 text-left">I&apos;m</span>
+          <span className="mr-5 text-pink-600">Warren</span>
+          <span className="text-cyan-400">Yun.</span>
+        </div>
+
+        <h1 className="text-3xl thin">
+          I&apos;m a student developer from New York ridiculously passionate
+          about technology, science, and everything in between.
+        </h1>
+      </div>
     </div>
   );
 }
