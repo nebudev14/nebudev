0addbea49beaaf8ce962aca97cc93bb39878f100
warren yun <wyun2006@gmail.com>
2022-04-03 21:57:28 -0400

    add spotify link to playing status

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,15 @@
 import Image from "next/image";
-import Social from "../components/Social";
+import Link from "next/link";
 
+import Social from "../components/Social";
 import axios from "axios";
 import { FaSpotify, FaMusic } from "react-icons/fa";
 
 export default function Home(props) {
   const songName = props.song;
   const isPlaying = props.isPlaying;
+  const link = props.link;
+
   return (
     <div className="flex items-center justify-center h-screen">
       <div className="flex flex-col items-center justify-start mr-12">
@@ -31,11 +34,28 @@ export default function Home(props) {
         </h1>
         <div className="flex flex-row items-center justify-start mt-3">
           <FaSpotify size={30} className="mr-2 text-green-400" />
-          {isPlaying ? <FaMusic size={20} className="mr-2 text-purple-500" /> : ''}
-          
-          {isPlaying ? <span>Currently listening to <u className="text-green-400">{songName}</u> </span> : <span>{songName}</span>}
-          
-          {isPlaying ? <FaMusic size={20} className="ml-2 text-purple-500" /> : ''}
+          {isPlaying ? (
+            <FaMusic size={20} className="mr-2 text-purple-500" />
+          ) : (
+            ""
+          )}
+
+          {isPlaying ? (
+            <span>
+              Currently listening to{" "}
+              <u className="text-green-400">
+                <Link href={link}>{songName}</Link>
+              </u>{" "}
+            </span>
+          ) : (
+            <span>{songName}</span>
+          )}
+
+          {isPlaying ? (
+            <FaMusic size={20} className="ml-2 text-purple-500" />
+          ) : (
+            ""
+          )}
         </div>
       </div>
     </div>
@@ -45,17 +65,19 @@ export default function Home(props) {
 export const getStaticProps = async () => {
   const getData = await axios.get("http://localhost:3000/api/spotify");
   const isPlaying = getData.data.data.body.is_playing;
+  const link = getData.data.data.body.item.external_urls.spotify;
   let song = {};
-  if(!isPlaying) {
-    song = "Not listening on Spotify right now."
+  if (!isPlaying) {
+    song = "Not listening on Spotify right now.";
   } else {
     song = getData.data.data.body.item.name;
   }
-  
+
   return {
     props: {
       isPlaying,
       song,
+      link,
     },
   };
 };
