26c81d1999e7adb43c40fbcda3b02b79a11bc474
warren yun <wyun2006@gmail.com>
2022-04-04 13:04:52 -0400

    add refresh token thing

--- pages/api/spotify.js
+++ pages/api/spotify.js
@@ -1,24 +1,32 @@
 const SpotifyWebApi = require("spotify-web-api-node");
 require("dotenv").config();
 
+var spotifyApi = new SpotifyWebApi();
+
+spotifyApi.setCredentials({
+  clientId: process.env.REACT_APP_CLIENT_ID,
+  clientSecret: process.env.REACT_APP_CLIENT_SECRET,
+  refreshToken: process.env.TOKEN,
+  redirectUri: 'http://localhost:3000/callback/',
+});
+
+
 export default function handler(req, res) {
   if (req.method == "GET") {
-    const spotify = new SpotifyWebApi({
-      clientId: process.env.REACT_APP_CLIENT_ID,
-      clientSecret: process.env.REACT_APP_CLIENT_SECRET,
-      redirectUri: process.env.REDIRECT_URI,
-    });
 
-    spotify.setAccessToken(process.env.TOKEN)
-    spotify
-      .getMyCurrentPlayingTrack()
-      .then(function (data) {
-        res.status(200).json({ data });
-      })
-      .catch(function (err) {
-        console.log("Error: " + err);
-        res.status(500);
-      });
+    spotifyApi.refreshAccessToken().then(function (data) {
+      // Set the access token on the API object so that it's used in all future requests
+      spotifyApi.setAccessToken(data.body["access_token"]);
+      spotifyApi
+        .getMyCurrentPlayingTrack()
+        .then(function (data) {
+          res.status(200).json({ data });
+        })
+        .catch(function (err) {
+          console.log("Error: " + err);
+          res.status(500);
+        });
+    });
   } else {
     res.status(400);
   }
