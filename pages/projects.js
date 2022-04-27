66d1f6536d5250d85624cf3babbdf1e74aa1fbeb
warren yun <wyun2006@gmail.com>
2022-04-27 00:24:21 -0400

    reformat projects page

--- pages/projects.js
+++ pages/projects.js
@@ -9,7 +9,17 @@ import {
 
 import { setContext } from "@apollo/client/link/context";
 import Link from "next/link";
-import { FaStar } from "react-icons/fa";
+import { FaStar, FaPython } from "react-icons/fa";
+
+const projects = [
+  {
+    "name": "Meetup Map API",
+    "stars": "1",
+    "desc": "Backend server for Meetup Map.",
+    "tags": ["Javascript", "GraphQL", "Prisma.Js", "PostgreSQL"],
+    "link": "https://github.com/MeetupMap/api"
+  }
+]
 
 export default function Projects(props) {
   return (
@@ -23,20 +33,27 @@ export default function Projects(props) {
       </h2>
       <span className="mb-6 text-xl thin md:text-lg">(Fetched using the Github API)</span>
       <div className="grid grid-cols-2 gap-16 my-10 md:grid-cols-1">
-        {props.repos.map((repo, i) => (
-          <Link key={i} href={repo.url} passHref>
-            <div className="px-6 py-4 duration-200 border border-black md:border-gray-600 hover:cursor-pointer hover:border-gray-600 rounded-3xl md:px-4 md:py-3">
+        {projects.map((repo, i) => (
+          <Link key={i} href={repo.link} passHref>
+            <div className="px-6 py-4 duration-200 border border-gray-400 hover:border-green-400 hover:cursor-pointer rounded-3xl md:px-4 md:py-3">
               <div className="pb-4 mb-4 border-b-4 border-green-400">
                 <h1 className="py-2 text-2xl">
-                  {repo.nameWithOwner}
+                  {repo.name}
                 </h1>
-                <div className="flex flex-row items-center justify-start">
+                <div className="flex flex-wrap mt-2">
+                  {repo.tags.map((tag, i) => (
+                    <div className="px-2 py-2 mb-3 mr-3 border border-green-500 rounded-xl" key={i}>
+                      {tag}
+                    </div>
+                  ))}
+                </div>
+                <div className="flex flex-row items-center justify-start mt-2">
                   <FaStar size={20} className="text-yellow-400"/>
-                  <h1 className="ml-2 text-lg">{repo.stargazerCount}</h1>
+                  <h1 className="ml-2 text-lg">{repo.stars}</h1>
                 </div>
               </div>
 
-              <span className="mb-8 text-xl thin">{repo.description}</span>
+              <span className="mb-8 text-xl thin">{repo.desc}</span>
             </div>
           </Link>
         ))}
