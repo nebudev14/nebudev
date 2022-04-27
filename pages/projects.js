65127774a09552166a1148e78494ca1e5a2fa51d
warren yun <wyun2006@gmail.com>
2022-04-27 00:59:31 -0400

    Update projects page

--- pages/projects.js
+++ pages/projects.js
@@ -1,24 +1,105 @@
-import {
-  ApolloClient,
-  InMemoryCache,
-  gql,
-  ApolloProvider,
-  useQuery,
-  createHttpLink,
-} from "@apollo/client";
-
-import { setContext } from "@apollo/client/link/context";
 import Link from "next/link";
-import { FaStar, FaPython } from "react-icons/fa";
+import { FaStar } from "react-icons/fa";
 
 const projects = [
   {
-    "name": "Meetup Map API",
+    "name": "MeetupMap-API",
     "stars": "1",
     "desc": "Backend server for Meetup Map.",
-    "tags": ["Javascript", "GraphQL", "Prisma.Js", "PostgreSQL"],
+    "tags": ["Javascript", "GraphQL", "Prisma.Js"],
     "link": "https://github.com/MeetupMap/api"
-  }
+  },
+  {
+    "name": "Lumiere",
+    "stars": "12",
+    "desc": "A media publication platform that accelerates CompSci content creators.",
+    "tags": ["Javascript", "Prisma.Js", "TailwindCSS"],
+    "link": "https://github.com/project-lumiere/lumiere.codes"
+  },
+  {
+    "name": "G.O.A.T-Notes",
+    "stars": "2",
+    "desc": "A chrome extension that allows you to easily take online notes during class, as well as access them afterwards. Made during HackOn 2.0.",
+    "tags": ["Javascript", "CSS", "Svelte"],
+    "link": "https://github.com/NebuDev14/G.O.A.T-Notes"
+  },
+  {
+    "name": "v3",
+    "stars": "1",
+    "desc": "Version 3 of my personal website.",
+    "tags": ["Javascript", "Next.Js", "TailwindCSS"],
+    "link": "https://github.com/NebuDev14/nebudev"
+  },
+  {
+    "name": "eduHub",
+    "stars": "3",
+    "desc": "A website dedicated to finding educational resources easily. Made during CodeOverflow 2022.",
+    "tags": ["Next.Js", "TailwindCSS", "MongoDB"],
+    "link": "https://github.com/NebuDev14/eduHub"
+  },
+  {
+    "name": "Control-Panel",
+    "stars": "1",
+    "desc": "An app designed for my tablet to interact with things on my computer.",
+    "tags": ["Flutter", "Python", "Flask"],
+    "link": "https://github.com/NebuDev14/Control-Panel"
+  },
+  {
+    "name": "AutoZoom",
+    "stars": "7",
+    "desc": "Automatically opens your zoom meetings/google meets at scheduled times.",
+    "tags": ["Python"],
+    "link": "https://github.com/NebuDev14/AutomaticZoom"
+  },
+  {
+    "name": "Diseasy-Peasy",
+    "stars": "2",
+    "desc": "Identify diseases based on what symptoms you have.",
+    "tags": ["Go", "Prisma.Js"],
+    "link": "https://github.com/NebuDev14/Diseasy-Peasy"
+  },
+  {
+    "name": "bxscioly.github.io",
+    "stars": "1",
+    "desc": "The website for Bronx Science's Scioly Team.",
+    "tags": ["React", "Styled Components"],
+    "link": "https://github.com/NebuDev14/bxscioly.github.io"
+  },
+  {
+    "name": "Shopping-Spree",
+    "stars": "1",
+    "desc": "A website that gathers the cheapest products for you to buy. Made during Explore Hacks 2021.",
+    "tags": ["Typescript", "Next.Js", "CSS"],
+    "link": "https://github.com/NebuDev14/Shopping-Spree"
+  },
+  {
+    "name": "TheraTracker",
+    "stars": "1",
+    "desc": "A website that tracks nearby therapists. Built for Mental Health Hacks 2021.",
+    "tags": ["React", "Python", "Flask"],
+    "link": "https://github.com/NebuDev14/TheraTracker"
+  },
+  {
+    "name": "Rona-Radar",
+    "stars": "1",
+    "desc": "An app that displays nearby hospitals and live updates on Covid-19. Won 3rd place at HackKU 2021.",
+    "tags": ["Java", "Android"],
+    "link": "https://github.com/Rona-Radar/app"
+  },
+  {
+    "name": "Music-Player",
+    "stars": "3",
+    "desc": "A circuit that simulates the track buttons on your keyboard. Won 1st place at Surprise Hacks 2021.",
+    "tags": ["Arduino"],
+    "link": "https://github.com/NebuDev14/Music-Player"
+  },
+  {
+    "name": "Auto-Fan",
+    "stars": "1",
+    "desc": "Automatically turns on a fan when the temperature is above a certain number. Made at Hack from the Past 2021.",
+    "tags": ["Arduino"],
+    "link": "https://github.com/NebuDev14/AutoFan"
+  },
 ]
 
 export default function Projects(props) {
@@ -31,7 +112,7 @@ export default function Projects(props) {
         Some of the <span className="text-green-400">projects</span> I&apos;ve
         worked on!
       </h2>
-      <span className="mb-6 text-xl thin md:text-lg">(Fetched using the Github API)</span>
+      <span className="mb-6 text-xl thin md:text-lg"></span>
       <div className="grid grid-cols-2 gap-16 my-10 md:grid-cols-1">
         {projects.map((repo, i) => (
           <Link key={i} href={repo.link} passHref>
@@ -42,8 +123,8 @@ export default function Projects(props) {
                 </h1>
                 <div className="flex flex-wrap mt-2">
                   {repo.tags.map((tag, i) => (
-                    <div className="px-2 py-2 mb-3 mr-3 border border-green-500 rounded-xl" key={i}>
-                      {tag}
+                    <div className="px-2 py-2 mb-3 mr-3 border border-purple-500 rounded-xl" key={i}>
+                      <p className="text-xs">{tag}</p>
                     </div>
                   ))}
                 </div>
@@ -61,57 +142,3 @@ export default function Projects(props) {
     </div>
   );
 }
-
-export const getStaticProps = async () => {
-  require("dotenv").config();
-
-  const httpLink = createHttpLink({
-    uri: "https://api.github.com/graphql",
-  });
-
-  const authLink = setContext((_, { headers }) => {
-    return {
-      headers: {
-        ...headers,
-        authorization: `Bearer ${process.env.GITHUB_TOKEN}`,
-      },
-    };
-  });
-
-  const client = new ApolloClient({
-    link: authLink.concat(httpLink),
-    cache: new InMemoryCache(),
-  });
-
-  const { data } = await client.query({
-    query: gql`
-      {
-        user(login: "NebuDev14") {
-          pinnedItems(first: 6) {
-            totalCount
-            edges {
-              node {
-                ... on Repository {
-                  id
-                  url
-                  stargazerCount
-                  description
-                  nameWithOwner
-                }
-              }
-            }
-          }
-        }
-      }
-    `,
-  });
-
-  const { user } = data;
-  const pinnedItems = user.pinnedItems.edges.map(({ node }) => node);
-
-  return {
-    props: {
-      repos: pinnedItems,
-    },
-  };
-};
