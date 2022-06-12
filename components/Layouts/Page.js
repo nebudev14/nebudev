bc4f67a5c72a2b346d00f861c8d2f774c863c043
warren yun <wyun2006@gmail.com>
2022-06-12 03:52:23 -0400

    better looking loading screen

--- components/Layouts/Page.js
+++ components/Layouts/Page.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import Image from "next/image";
 import Router from "next/router";
 import { AnimatePresence, motion } from "framer-motion";
 
@@ -11,31 +12,40 @@ export default function Page({ children }) {
     exit: { opacity: 0, x: 0, y: -100 },
   };
 
-//   Router.onRouteChangeStart = (url) => {
-//     setLoading(true);
-//   };
+    Router.onRouteChangeStart = (url) => {
+      setLoading(true);
+    };
 
-//   Router.onRouteChangeComplete = (url) => {
-//     setLoading(false);
-//   };
+    Router.onRouteChangeComplete = (url) => {
+      setLoading(false);
+    };
 
   return (
     <div className="px-8 md:flex md:flex-col">
       {loading ? (
-        <div className="flex-col items-center justify-center h-screen">
+        <div className="">
           <AnimatePresence exitBeforeEnter>
-          <motion.div
-            initial="hidden"
-            animate="enter"
-            exit="exit"
-            variants={variants}
-            transition={({ type: "linear" }, { duration: 0.4 })}
-            
-          >
-            <div></div>
-          </motion.div>
-          <h1>hello</h1>
-        </AnimatePresence>
+            <motion.div
+              initial="hidden"
+              animate="enter"
+              exit="exit"
+              variants={variants}
+              transition={({ type: "linear" }, { duration: 0.4 })}
+            >
+              <div className="flex flex-col items-center justify-center h-screen">
+                <Image
+                    src="/warrenLoading.jpg"
+                    alt="warren loading"
+                    width={160}
+                    height={160}
+                    className="rounded-full"
+                />
+                <h1 className="p-4 mt-6 text-6xl text-center text-pink-600 border-b-4 border-cyan-400">
+                  Loading...
+                </h1>
+              </div>
+            </motion.div>
+          </AnimatePresence>
         </div>
       ) : (
         children
