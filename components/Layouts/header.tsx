1220e11f07fb8d1d41c1c8748deb81e9228138c9
warren yun <wyun2006@gmail.com>
2022-08-15 14:23:36 -0400

    show selected item on nav

--- components/Layouts/header.tsx
+++ components/Layouts/header.tsx
@@ -1,9 +1,9 @@
 import { CgHome, CgProfile } from "react-icons/cg";
 import { GoProject } from "react-icons/go";
 import { FaProjectDiagram } from "react-icons/fa";
+import { useState } from "react";
 
 import Link from "next/link";
-
 
 const icons = [
   {
@@ -25,17 +25,23 @@ const icons = [
 ];
 
 export const Header: React.FC = () => {
+  const [selected, setSelcted] = useState(0);
 
   return (
     <div className="fixed z-50 flex flex-col items-center justify-center h-screen mt-4 text-white md:flex-row md:h-full md:relative">
       {icons.map((icon, i) => (
         <div
           key={i}
-          className="mb-8 duration-200 hover:text-pink-600 hover:cursor-pointer md:mb-0 md:mx-3"
+          className={`mb-6 px-2 py-1 border-l-4 duration-200 hover:text-cyan-400 hover:cursor-pointer md:mb-0 md:mx-3 ${
+            selected === i ? "border-pink-600" : "border-black"
+          }`}
+          onClick={() => setSelcted(i)}
         >
-          <Link href={icon.link} scroll={false}>{icon.icon}</Link>
+          <Link href={icon.link} scroll={false}>
+            {icon.icon}
+          </Link>
         </div>
       ))}
     </div>
   );
-}
+};
