a170a8c94f3f4fbc4ce1f838a519387b86283a9e
warren yun <wyun2006@gmail.com>
2023-07-22 01:41:18 -0400

    reformat skills

--- components/skills.tsx
+++ components/skills.tsx
@@ -1,7 +1,172 @@
+import { NextPage } from "next";
+import {
+  SiJavascript,
+  SiPython,
+  SiArduino,
+  SiPostgresql,
+  SiPrisma,
+  SiNodedotjs,
+  SiTailwindcss,
+  SiGit,
+  SiHtml5,
+  SiCss3,
+  SiReact,
+  SiFlutter,
+  SiNextdotjs,
+  SiTypescript,
+  SiGraphql,
+  SiDocker,
+} from "react-icons/si";
+import { BiLogoGoLang, BiLogoJava } from "react-icons/bi";
+
+const skills = [
+  {
+    name: "Typescript",
+    icon: <SiTypescript size={65} />,
+    year: "2022",
+    color: "border-cyan-600",
+  },
+  {
+    name: "Go",
+    icon: <BiLogoGoLang size={65} />,
+    year: "2021",
+    color: "border-cyan-400",
+  },
+  {
+    name: "Python",
+    icon: <SiPython size={65} />,
+    year: "2020",
+    color: "border-cyan-500",
+  },
+  {
+    name: "Javascript",
+    icon: <SiJavascript size={65} />,
+    year: "2021",
+    color: "border-yellow-400",
+  },
+  {
+    name: "Java",
+    icon: <BiLogoJava size={65} />,
+    year: "2020",
+    color: "border-orange-500",
+  },
+  {
+    name: "Node.js",
+    icon: <SiNodedotjs size={65} />,
+    year: "2021",
+    color: "border-green-400",
+  },
+  {
+    name: "Arduino",
+    icon: <SiArduino size={65} />,
+    year: "2020",
+    color: "border-cyan-300",
+  },
+  {
+    name: "PostgreSQL",
+    icon: <SiPostgresql size={65} />,
+    year: "2021",
+    color: "border-blue-400",
+  },
+  {
+    name: "HTML",
+    icon: <SiHtml5 size={65} />,
+    year: "2020",
+    color: "border-orange-400",
+  },
+  {
+    name: "CSS",
+    icon: <SiCss3 size={65} />,
+    year: "2020",
+    color: "border-cyan-400",
+  },
+];
+
+const frameworks = [
+  {
+    name: "Next.Js",
+    icon: <SiNextdotjs size={65} />,
+    year: "2021",
+    color: "",
+  },
+  {
+    name: "TailwindCSS",
+    icon: <SiTailwindcss size={65} />,
+    year: "2021",
+    color: "border-cyan-400",
+  },
+  {
+    name: "Prisma",
+    icon: <SiPrisma size={65} />,
+    year: "2021",
+    color: "border-cyan-600",
+  },
+  {
+    name: "GraphQL",
+    icon: <SiGraphql size={65} />,
+    year: "2021",
+    color: "border-pink-500",
+  },
+  {
+    name: "Docker",
+    icon: <SiDocker size={65} />,
+    year: "2021",
+    color: "border-cyan-400",
+  },
+  {
+    name: "Git",
+    icon: <SiGit size={65} />,
+    year: "2020",
+    color: "border-orange-600",
+  },
+  {
+    name: "React",
+    icon: <SiReact size={65} />,
+    year: "2022",
+    color: "border-cyan-400",
+  },
+  {
+    name: "Flutter",
+    icon: <SiFlutter size={65} />,
+    year: "2021",
+    color: "border-cyan-300",
+  },
+];
+
 export const Skills: React.FC = () => {
   return (
-    <div>
-      
+    <div className="w-full h-full px-16 mt-8 md:px-0 pb-14">
+      <div className="flex items-end justify-center">
+        <h1 className="inline-block px-2 py-2 mb-12 ml-auto font-mono text-2xl font-bold text-right border-b-4 md:py-3 md:text-xl md:text-center border-b-cyan-400">
+          skills!
+        </h1>
+      </div>
+      <h2 className="mb-6 text-3xl">Languages</h2>
+      <div className="grid grid-cols-2 gap-12 mb-10 md:grid-cols-1">
+        {skills.map((skill, i) => (
+          <div
+            key={i}
+            className={`px-10 py-6 border-2 rounded-3xl ${skill.color}`}
+          >
+            <h1 className="mb-3 text-5xl md:text-2xl">{skill.name}</h1>
+            <span className="">{skill.year}</span>
+            <div className="mt-4">{skill.icon}</div>
+          </div>
+        ))}
+      </div>
+      <h2 className="my-6 text-3xl">Frameworks/Tools</h2>
+      <div className="grid grid-cols-2 gap-12 mb-10 md:grid-cols-1">
+        {frameworks.map((skill, i) => (
+          <div
+            key={i}
+            className={`px-10 py-6 border-2 rounded-3xl ${skill.color}`}
+          >
+            <h1 className="mb-3 text-5xl md:text-2xl">{skill.name}</h1>
+            <span className="">{skill.year}</span>
+            <div className="mt-4">{skill.icon}</div>
+          </div>
+        ))}
+      </div>
     </div>
   );
-}
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -6,6 +6,7 @@ import axios from "axios";
 import { FaSpotify, FaMusic } from "react-icons/fa";
 import { GetServerSideProps, NextPage } from "next";
 import { Timeline } from "../components/timeline";
+import { Skills } from "../components/skills";
 
 interface Props {
   song: string;
@@ -19,77 +20,85 @@ const Home: NextPage<Props> = (props) => {
   const link = props.link;
 
   return (
-    <div className="flex flex-col items-center justify-center">
-      <div className="flex items-center justify-center h-screen md:mt-16 md:flex-col md:justify-start">
-        <div className="flex flex-col items-center justify-center mr-12 md:mr-0 md:mb-6">
-          <h1 className="py-1 mb-4 font-mono text-sm text-gray-200 duration-150 border-b-4 border-gray-600 hover:cursor-pointer hover:border-green-500 hover:-translate-y-2">
-            view my resume!
-          </h1>
-          <Image
-            src="/warren.jpeg"
-            alt="warren"
-            width={275}
-            height={275}
-            className="rounded-3xl"
-          />
-        </div>
-        <div className="w-1/2 md:w-full">
-          <Social />
-          <div className="inline-flex text-8xl md:text-3xl">
-            <span className="mr-5 text-left md:mr-3">I&apos;m</span>
-            <span className="mr-5 text-pink-600 md:mr-3">Warren</span>
-            <span className="text-cyan-400">Yun.</span>
+    <>
+      <div className="flex flex-col items-center justify-center">
+        <div className="flex items-center justify-center h-screen md:mt-16 md:flex-col md:justify-start">
+          <div className="flex flex-col items-center justify-center mr-12 md:mr-0 md:mb-6">
+            <h1 className="py-1 mb-4 font-mono text-sm text-gray-200 duration-150 border-b-4 border-gray-600 hover:cursor-pointer hover:border-green-500 hover:-translate-y-2">
+              view my resume!
+            </h1>
+            <Image
+              src="/warren.jpeg"
+              alt="warren"
+              width={275}
+              height={275}
+              className="rounded-3xl"
+            />
           </div>
-          <h1 className="inline-block m-0 font-mono text-4xl md:text-lg ">
-            I&apos;m an avid maker from New York interested in{" "}
-            <span className="hover:text-yellow-400">robotics</span>,{" "}
-            <span className="hover:text-purple-500">web development</span>, and{" "}
-            <span className="hover:text-cyan-700">embedded security</span>.
-          </h1>
-          <div className="flex flex-row items-center justify-start mt-3 md:text-sm md:flex-col md:items-start">
-            <FaSpotify size={30} className="mr-2 text-green-400 md:hidden" />
-            {isPlaying ? (
-              <FaMusic size={20} className="mr-2 text-purple-500 md:hidden" />
-            ) : (
-              ""
-            )}
+          <div className="w-1/2 md:w-full">
+            <Social />
+            <div className="inline-flex text-8xl md:text-3xl">
+              <span className="mr-5 text-left md:mr-3">I&apos;m</span>
+              <span className="mr-5 text-pink-600 md:mr-3">Warren</span>
+              <span className="text-cyan-400">Yun.</span>
+            </div>
+            <h1 className="inline-block m-0 font-mono text-4xl md:text-lg ">
+              I&apos;m an avid maker from New York interested in{" "}
+              <span className="hover:text-yellow-400">robotics</span>,{" "}
+              <span className="hover:text-purple-500">web development</span>,
+              and <span className="hover:text-cyan-700">embedded security</span>
+              .
+            </h1>
+            <div className="flex flex-row items-center justify-start mt-3 md:text-sm md:flex-col md:items-start">
+              <FaSpotify size={30} className="mr-2 text-green-400 md:hidden" />
+              {isPlaying ? (
+                <FaMusic size={20} className="mr-2 text-purple-500 md:hidden" />
+              ) : (
+                ""
+              )}
 
-            {isPlaying ? (
-              <span className="md:mt-2">
-                <div className="inline-block mr-2 md:block">
-                  Currently jamming to{" "}
-                </div>
-                <span>
-                  <u className="font-mono text-green-400">
-                    <Link href={link}>{songName}</Link>
-                  </u>{" "}
-                  <FaSpotify
-                    size={25}
-                    className="hidden mr-2 text-green-400 md:inline md:ml-1"
-                  />
+              {isPlaying ? (
+                <span className="md:mt-2">
+                  <div className="inline-block mr-2 md:block">
+                    Currently jamming to{" "}
+                  </div>
+                  <span>
+                    <u className="font-mono text-green-400">
+                      <Link href={link}>{songName}</Link>
+                    </u>{" "}
+                    <FaSpotify
+                      size={25}
+                      className="hidden mr-2 text-green-400 md:inline md:ml-1"
+                    />
+                  </span>
                 </span>
-              </span>
-            ) : (
-              <span>{songName}</span>
-            )}
+              ) : (
+                <span>{songName}</span>
+              )}
 
-            {isPlaying ? (
-              <FaMusic size={20} className="ml-2 text-purple-500 md:hidden" />
-            ) : (
-              ""
-            )}
+              {isPlaying ? (
+                <FaMusic size={20} className="ml-2 text-purple-500 md:hidden" />
+              ) : (
+                ""
+              )}
+            </div>
           </div>
+        </div>
+        <h1 className="font-mono text-lg text-gray-400 mb-28 ">
+          <span className="md:hidden">business inquiries? reach me at</span>{" "}
+          <span className="font-extrabold text-gray-300">
+            hi [at] wyun [dot] dev
+          </span>
+        </h1>
+
+        <div>
+          <Timeline />
         </div>
       </div>
-      <h1 className="font-mono text-lg text-gray-400 mb-28 ">
-        <span className="md:hidden">business inquiries? reach me at</span>{" "}
-        <span className="font-extrabold text-gray-300">
-          hi [at] wyun [dot] dev
-        </span>
-      </h1>
-
-      <div><Timeline /></div>
-    </div>
+      <div>
+        <Skills />
+      </div>
+    </>
   );
 };
 
